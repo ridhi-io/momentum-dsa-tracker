656c4490e7c60ba8b16031ecfc2fb8e05cde1789
ridhi-io <ridhi.24bcy10260@vitbhopal.ac.in>
2026-07-04 23:38:11 +0530

    Connect frontend to deployed backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://momentum-dsa-tracker-production-cd9e.up.railway.app",
 });
 
 export default API;
