4532bd6799b43f222a5f220681f15d9c3805e01c
ridhi-io <ridhi.24bcy10260@vitbhopal.ac.in>
2026-07-04 23:00:29 +0530

    Use Railway PORT

--- server/server.js
+++ server/server.js
@@ -65,6 +65,10 @@ app.delete("/problems/:id", (req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log("🚀 Server Running on Port 5000");
+const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server Running on Port ${PORT}`);
 });
